ea6ff1fc5e187e9b15176da3305b38d76241bef9
unknown <thibaut.constant@isisphinx.com>
2020-04-23 19:26:59 +0200

    add a function de copy players directory from the first 1

---
+++ replicatePlayers.cmd
@@ -0,0 +1,7 @@
+REM Delete players and copy over player 0Ujzu57VXWwJTB5erTUp
+rmdir /s /q src\pages\12345\GpBYQ4vqkiEImQrbkkHv
+rmdir /s /q src\pages\12345\NvysJ1bND6X1RONVG3Yu
+rmdir /s /q src\pages\12345\yhSG30Rf9lB0Me9sLoRS
+Xcopy /E /I src\pages\12345\0Ujzu57VXWwJTB5erTUp src\pages\12345\GpBYQ4vqkiEImQrbkkHv
+Xcopy /E /I src\pages\12345\0Ujzu57VXWwJTB5erTUp src\pages\12345\NvysJ1bND6X1RONVG3Yu
+Xcopy /E /I src\pages\12345\0Ujzu57VXWwJTB5erTUp src\pages\12345\yhSG30Rf9lB0Me9sLoRS
--- src/pages/12345/yhSG30Rf9lB0Me9sLoRS/players/index.js
+++ src/pages/12345/yhSG30Rf9lB0Me9sLoRS/players/index.js
@@ -3,8 +3,8 @@ import firebase from "gatsby-plugin-firebase";
 
 import PlayersList from '../../../../components/playersList'
 
+import {players} from '../../index'
 
-const players = [{ id: 'NvysJ1bND6X1RONVG3Yu', name: 'MJ' }, { id: '0Ujzu57VXWwJTB5erTUp', name: 'Günther Olsen' }, { id: 'GpBYQ4vqkiEImQrbkkHv', name: 'Arakel Sarif' }, { id: 'yhSG30Rf9lB0Me9sLoRS', name: 'Jean Test' }]
 const nullResults = { 'NvysJ1bND6X1RONVG3Yu': { diceResult: null, dice: null }, '0Ujzu57VXWwJTB5erTUp': { diceResult: null, dice: null }, 'GpBYQ4vqkiEImQrbkkHv': { diceResult: null, dice: null }, 'yhSG30Rf9lB0Me9sLoRS': { diceResult: null, dice: null } }
 
 export default function SimpleList({ location }) {
