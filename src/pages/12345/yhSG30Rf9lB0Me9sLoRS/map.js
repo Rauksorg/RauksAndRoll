df1ba65dca5493097e3fb53bc110593afa92d380
unknown <thibaut.constant@isisphinx.com>
2020-04-25 00:07:31 +0200

    improve map

--- src/pages/12345/yhSG30Rf9lB0Me9sLoRS/map.js
+++ src/pages/12345/yhSG30Rf9lB0Me9sLoRS/map.js
@@ -1,25 +1,42 @@
 import React from "react";
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
+
+const iframe = `<iframe width="100%" height="600px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/lenzen-rauks_449329?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=null&onLoadPanel=undefined&captionBar=false#15/53.0915/11.4755" />`
+
+const Iframe = ({ iframe }) => {
+  return (<div dangerouslySetInnerHTML={{ __html: iframe ? iframe : "" }} />);
+}
 
 export default ({ data }) => {
   return (
     <div>
-      <Img fixed={data.file.childImageSharp.fixed} />
+      <Iframe iframe={iframe}/>
+      {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
     </div>
   );
 }
 
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "LenzenMap.jpg" }) {
-      childImageSharp  {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed(width: 1097, height: 591, quality: 90) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`
+// import Img from "gatsby-image";
+// import { graphql } from "gatsby";
+
+// export default ({ data }) => {
+//   return (
+//     <div>
+//       <Img fixed={data.file.childImageSharp.fixed} />
+//     </div>
+//   );
+// }
+
+
+// export const query = graphql`
+//   query {
+//     file(relativePath: { eq: "LenzenMap.jpg" }) {
+//       childImageSharp  {
+//         # Specify the image processing specifications right in the query.
+//         # Makes it trivial to update as your page's design changes.
+//         fixed(width: 1097, height: 591, quality: 90) {
+//           ...GatsbyImageSharpFixed
+//         }
+//       }
+//     }
+//   }
+// `
