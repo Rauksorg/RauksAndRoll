0bcb6d19f7dcac681a8fffe5ef3e7e12881e35ac
Thibaut Constant <thibaut.constant@isisphinx.com>
2020-05-08 01:19:45 +0200

    add dice button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link } from "gatsby-theme-material-ui";
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import { EpicFailIcon, FailIcon, SuccessIcon, TwoIcon, FourIcon, ThreeEpicIcon, ExplosivIcon, OptionIcon } from "../components/diceIcons";
 
-export default () => (
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+  return (
+
+
     <div>
       <Link to="/12345/">
         Go to game 12345
       </Link>
+      <div>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><EpicFailIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><FailIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><TwoIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><FourIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><SuccessIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><ThreeEpicIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><ExplosivIcon fontSize="large" /></Avatar>
+        <Avatar className={classes.large} style={{ backgroundColor: "blue" }}><OptionIcon fontSize="large" /></Avatar>
+      </div>
     </div>
   );
-
-// const text = `╔═══Caractéristiques
-// ║ 💪 : 3 | 👁 : 3 | 🧠 : 3 | 🕶 : 3
-// ║ 📖 : 3 | 🍀 : 3
-// ╠═══Compétences
-// ║ -
-// ║ -
-// ║ -
-// ║ -
-// ╠═══Traits
-// ║ -
-// ║ -
-// ║ -
-// ╠═══Traumatismes
-// ║ -
-// ║ -
-// ╚═══`
+}
+// const text = `3💪 | 3👁 | 3🧠 | 3🕶 ||| 3📖 | 3🍀`
