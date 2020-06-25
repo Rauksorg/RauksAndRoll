1a6a867a4a71dcd705cf98420e873ebfae84f265
unknown <thibaut.constant@isisphinx.com>
2020-06-25 18:52:08 +0200

    finished to convert character to redux

--- src/pages/games/aramis/character.js
+++ src/pages/games/aramis/character.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef, useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import firebase from 'gatsby-plugin-firebase'
 import { makeStyles, withStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
@@ -68,7 +68,7 @@ const marks = [
   { value: 8, label: '8' },
   { value: 9, label: '9' },
 ]
-// find a way to include identification field without mnessing with render
+
 const inputsFields = [
   { field: 'attributes', title: '' },
   { field: 'skills', title: 'Skills' },
@@ -167,6 +167,19 @@ const StatusDesc = ({ title, field, gameId, playerId }) => {
   )
 }
 
+const Status = ({ field, gameId, playerId }) => {
+  const dispatch = useDispatch()
+  const fieldValue = useSelector((state) => state.playersList[playerId][field])
+
+  const handleChange = (_, newValue) => {
+    if (newValue === null) return
+    dispatch(modifyField({ value: newValue, field: field, playerId: playerId }))
+    updateFieldInDb(gameId, playerId, field, newValue)
+  }
+
+  return <StyledRating name='customized-icons' max={3} value={fieldValue} onChange={handleChange} getLabelText={(value) => customIcons[value].label} IconContainerComponent={IconContainer} />
+}
+
 const Character = ({ location }) => {
   const classes = useStyles()
   const loading = useSelector((state) => state.loading)
@@ -187,142 +200,13 @@ const Character = ({ location }) => {
           </div>
         ))}
         <StatusDesc title='Etats' field='statusDesc' playerId={playerId} gameId={gameId} />
+        <div className={classes.top10px}>
+          <Status field='status' playerId={playerId} gameId={gameId} />
+        </div>
       </form>
     </Paper>
   ) : (
     'Loading...'
-  )
-}
-
-const CharacterOld = ({ location }) => {
-  const classes = useStyles()
-  const timer = useRef(null)
-  const playerId = location.pathname.split('/')[2]
-
-  const [reroll, setReroll] = useState(null)
-  const [status, setStatus] = useState(null)
-  const [statusText, setStatusText] = useState('')
-  const [statusChip, setStatusChip] = useState([])
-
-  const [userInput, setUserInput] = useReducer((state, newState) => ({ ...state, ...newState }), {
-    identification: null,
-    attributes: null,
-    skills: null,
-    perks: null,
-    traumas: null,
-    notes: null,
-  })
-
-  // underscor debounce
-  const handleChange = (evt) => {
-    clearTimeout(timer.current)
-    const name = evt.target.name
-    const newValue = evt.target.value
-    setUserInput({ [name]: newValue })
-    timer.current = setTimeout(() => {
-      firebase
-        .firestore()
-        .doc(`players/${playerId}`)
-        .update({
-          [name]: newValue,
-        })
-    }, 400)
-  }
-
-  useEffect(() => {
-    const unsubscribe = firebase
-      .firestore()
-      .doc(`players/${playerId}`)
-      .onSnapshot((doc) => {
-        const data = doc.data()
-        if (!doc.metadata.hasPendingWrites) {
-          inputsFields.forEach((element) => {
-            setUserInput({ [element.name]: doc.data()[element.name] })
-          })
-          setUserInput({ identification: doc.data().identification })
-        }
-        setReroll(data.reroll)
-        setStatusChip(data.statusDesc)
-        setStatus(data.status)
-      })
-    return unsubscribe
-  }, [playerId])
-
-  const handleRerollChange = (_, newValue) => {
-    firebase.firestore().doc(`players/${playerId}`).update({
-      reroll: newValue,
-    })
-  }
-  const handleStatusChange = (event, newValue) => {
-    if (newValue === null) return
-    setStatus(newValue)
-    firebase.firestore().doc(`players/${playerId}`).update({
-      status: newValue,
-    })
-  }
-  const handleStatusChipChange = (event, newValue) => {
-    setStatusChip(newValue)
-    setStatusText('')
-    firebase.firestore().doc(`players/${playerId}`).update({ statusDesc: newValue })
-  }
-  const handleStatusTextChange = (event) => {
-    setStatusText(event.target.value)
-  }
-  const handleStatusTextBlur = () => {
-    if (!statusText) return
-    setStatusChip([...statusChip, statusText])
-    setStatusText('')
-    firebase
-      .firestore()
-      .doc(`players/${playerId}`)
-      .update({ statusDesc: [...statusChip, statusText] })
-  }
-  return (
-    <Paper style={{ padding: '15px', margin: '5px 15px 5px 15px' }}>
-      <form noValidate autoComplete='off'>
-        <TextField name='identification' label='Id' multiline value={userInput.identification != null ? userInput.identification : '...'} onChange={handleChange} variant='outlined' className={classes.root} />
-        <Typography id='discrete-slider-custom' gutterBottom style={{ marginTop: '10px' }}>
-          Relances
-        </Typography>
-        <Slider value={reroll != null ? reroll : 5} onChange={handleRerollChange} aria-labelledby='discrete-slider-custom' step={1} valueLabelDisplay='auto' marks={marks} max={9} />
-        {inputsFields.map((element, i) => {
-          const value = userInput[element.name]
-          return (
-            <TextField
-              key={i}
-              name={element.name}
-              label={element.title}
-              multiline
-              value={value != null ? value : '...'}
-              onChange={handleChange}
-              variant='outlined'
-              className={classes.root}
-              style={{ marginTop: '10px' }}
-            />
-          )
-        })}
-        <Autocomplete
-          multiple
-          id='tags-filled'
-          style={{ marginTop: '10px' }}
-          options={[]}
-          value={statusChip}
-          onChange={handleStatusChipChange}
-          freeSolo
-          renderTags={(value, getTagProps) => value.map((option, index) => <Chip variant='outlined' label={option} {...getTagProps({ index })} />)}
-          renderInput={(params) => <TextField value={statusText} onChange={handleStatusTextChange} onBlur={handleStatusTextBlur} variant='outlined' {...params} label='Etats' placeholder='Ajouter...' />}
-        />
-        <StyledRating
-          name='customized-icons'
-          max={3}
-          value={status}
-          style={{ marginTop: '10px' }}
-          onChange={handleStatusChange}
-          getLabelText={(value) => customIcons[value].label}
-          IconContainerComponent={IconContainer}
-        />
-      </form>
-    </Paper>
   )
 }
 
